04cb53f48c301a4b1493f9aad816cb9bc4cacb75
RealNikolai <33905094+RealNikolai@users.noreply.github.com>
2017-11-22 17:11:43 +0100

    Update bot.js

--- bot.js
+++ bot.js
@@ -11,5 +11,13 @@ client.on('message', message => {
   	}
 });
 
+
+  if(command === "pr!ping") {
+    const m = await message.channel.send("Ping?");
+    m.edit(`Pong! Latency is ${m.createdTimestamp - message.createdTimestamp}ms. API Latency is ${Math.round(client.ping)}ms`);
+  }
+
+
+
 // THIS  MUST  BE  THIS  WAY
 client.login(process.env.BOT_TOKEN);
